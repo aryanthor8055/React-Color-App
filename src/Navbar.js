985de9f9b296545f63289c00c84a70538a66715d
Aryan <aryan16072001236809@gmail.com>
2022-12-08 17:19:44 +0530

    Add Select and Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import 'rc-slider/assets/index.css';
 import Slider, { Range } from 'rc-slider';
+import MenuItem from '@mui/material/MenuItem';
 import './Navbar.css'
+// import { MenuItem } from '@mui/material';
+import { hex, rgb } from 'chroma-js';
 
 class Navbar extends Component {
-   
+  constructor(props){
+    super(props);
+    this.state={format:"hex"};
+    this.handleChange=this.handleChange.bind(this);
+  }
+   handleChange(e){
+    this.setState({format:e.target.value})
+    this.props.handleChange(e.target.value)
+   }
   render() {
-    const {level,changeLevel}=this.props;
+    const {level,changeLevel,handleChange}=this.props;
+    const {format}=this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
@@ -22,9 +35,17 @@ class Navbar extends Component {
         onAfterChange={changeLevel}/>
         </div>
         </div>
-       
+       <div className='select-container'>
+        <Select value={format} onChange={this.handleChange}>
+          <MenuItem value="hex">HEX - #fffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0) </MenuItem>
+        </Select>
+       </div>
       </header>
     )
   }
 }
 export default Navbar;
+
+
--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -22,7 +22,7 @@ for(let i in scale){
         id:color.name.toLowerCase().replace(/ /g,"_"),
         hex:scale[i],
         rgb:chroma(scale[i]).css(),
-        rgba:chroma(scale[i]).css().replace("rgba","rgba").replace(")",",1.0)")
+        rgba:chroma(scale[i]).css().replace("rgb","rgba").replace(")",",1.0)")
     });
 }
 }
