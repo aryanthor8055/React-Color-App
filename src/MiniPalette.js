26d044bdb81df47c97a981a662d180834c556281
Aryan <aryan16072001236809@gmail.com>
2022-12-09 21:11:13 +0530

    Finish Styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -14,13 +14,18 @@ overflow:"hidden",
 }
 },
 colors:{
-backgroundColor:"grey"
+backgroundColor:"#dea1e4",
+height:"150px",
+width:"100%",
+borderRadius:"5px",
+overflow:"hidden"
 },
 title:{
 display:"flex",
 justifyContent:"space-between",
-alignItems:"center",
+alignItems: "center",
 margin:"0",
+alignItems:"center",
 color:"black",
 paddingTop:"0.5rem",
 fontSize:"1rem",
@@ -29,14 +34,32 @@ position:"relative"
 emoji:{
 marginLeft:"0.5rem",
 fontSize:"1.5rem"
+},
+
+miniColor:{
+    height:"25%",
+    width:"20%",
+    display:"inline-block",
+    margin:"0 auto",
+    position:"relative",
+    marginBottom:"-3.5px"
 }
 };
 
 function MiniPalette(props){
-    const {classes,paletteName,emoji}=props;
+    const {classes,paletteName,emoji,colors}=props;
+    const minicolorBoxes=colors.map(color=>(
+        <div className={classes.miniColor} 
+        style={{backgroundColor:color.color}} 
+        key={color.name}>
+
+        </div>
+    ))
     return(
         <div className={classes.root}>
-         <div className={classes.colors}></div>
+         <div className={classes.colors}>
+            {minicolorBoxes}
+         </div>
          <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span></h5>
         </div>
     )
