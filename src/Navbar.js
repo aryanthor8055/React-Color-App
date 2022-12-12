d374689696aa393c95c40fbffeb89d84dd60d34d
Aryan <aryan16072001236809@gmail.com>
2022-12-12 23:41:06 +0530

    Add Navbar and Footer to SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,14 +27,14 @@ class Navbar extends Component {
     this.setState({open:false});
    }
   render() {
-    const {level,changeLevel}=this.props;
+    const {level,changeLevel,showingAllColors}=this.props;
     const {format}=this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
           <Link to='/'>reactcolorpicker</Link>
         </div>
-        <div className='slider-container'>
+       {showingAllColors && (<div className='slider-container'>
             <span>Level:{level}</span>
         <div className='slider'>
         <Slider defaultValue={level} 
@@ -44,6 +44,7 @@ class Navbar extends Component {
         onAfterChange={changeLevel}/>
         </div>
         </div>
+  )}
        <div className='select-container'>
         <Select value={format} onChange={this.handleFormatChange}>
           <MenuItem value="hex">HEX - #fffff</MenuItem>
--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ColorBox from './ColorBox';
 import './Palette.css'
 import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 
 class Palette extends Component {
   constructor(props){
@@ -29,17 +30,14 @@ this.setState({level})
     ))
     return (
       <div className='Palette'>
-       <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
+       <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} showingAllColors={true}/>
    {/* Navbar Goes here */}
    <div className='Palette-colors'>
     {/* bunch of color boxes */}
         {colorBoxes}
    
    </div>
-    <footer className='Palette-footer'>
-      {paletteName}
-      <span className='emoji'>{emoji}</span>
-    </footer>
+   <PaletteFooter paletteName={paletteName} emoji={emoji}/>
       </div>
     )
   }
